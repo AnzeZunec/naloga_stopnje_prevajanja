7351a1699e4062d09440f86597df91016061384a
Anze Zunec <anze.zunec1@student.um.si>
2026-03-22 18:39:11 +0000

    Added function printHello

--- main.c
+++ main.c
@@ -2,8 +2,13 @@
 
 #define EXAMPLENUM 25
 
+void printHello() {
+    printf("Hello World");
+}
+
 int main() {
   
     printf("Example Number: %d", EXAMPLENUM);
+    printHello();
     return 0;
 }
